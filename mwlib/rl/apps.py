d0a78ea07051297d1ad8ed5319784ea9441e885f
ralf@brainbot.com <ralf@brainbot.com>
2008-04-23 16:23:23 +0200

    show tracebacks, use util.start_logging

--- mwlib/rl/apps.py
+++ mwlib/rl/apps.py
@@ -11,8 +11,7 @@ from ConfigParser import ConfigParser
 
 import mwlib.log
 from mwlib.metabook import MetaBook
-from mwlib.utils import daemonize
-from mwlib import parser, uparser
+from mwlib import parser, uparser, utils
 
 log = mwlib.log.Log('mw-app-rl')
 
@@ -41,7 +40,8 @@ OR
     optparser.add_option("-r", "--removedarticles", help="list of articles that were removed b/c rendering was impossible")
     optparser.add_option("-d", "--daemonize", action="store_true", dest="daemonize", default=False,
                       help="return immediately and generate PDF in background")
-    optparser.add_option("-e", "--errorfile", help="write caught errors to this file")
+    
+    optparser.add_option("-e", "--errorfile", help="write caught errors to this file") # FIXME: currently ignored, remove this later
     options, args = optparser.parse_args()
     
     if not args and not (options.metabookfile and options.output):
@@ -54,8 +54,8 @@ OR
     logfile = options.logfile
 
     if logfile:
-        mwlib.log.Log.logfile = (open(logfile,"w"))
-
+        utils.start_logging(logfile)
+        
     if not config:
         try:
             config = os.environ['MWPDFCONFIG']
@@ -63,53 +63,46 @@ OR
             sys.exit('Please specify config file location with --config or set environment variable MWPDFCONFIG')
     
     if options.daemonize:
-        daemonize()
+        utils.daemonize()
     
-    try:
-        from mwlib import wiki
-        from mwlib.rl import rlwriter
-        w = wiki.makewiki(config)
+    from mwlib import wiki
+    from mwlib.rl import rlwriter
+    w = wiki.makewiki(config)
 
-        r=rlwriter.RlWriter(images=w['images'] )    
-        metabook = MetaBook()
-        cp=ConfigParser()
-        cp.read(config)
+    r=rlwriter.RlWriter(images=w['images'])    
+    metabook = MetaBook()
+    cp=ConfigParser()
+    cp.read(config)
 
-        licensearticle = cp.get('wiki', 'defaultarticlelicense')
-        if licensearticle:
-            license = uparser.parseString(title=licensearticle, wikidb=w['wiki'])
-        metabook.source = {
-            'name': cp.get('wiki', 'name'),
-            'url': cp.get('wiki', 'url'),
-            'defaultarticlelicense': license,
-        }    
-        coverimage = None
-        if not metabookfile:
-            article = unicode(args[0],'utf-8').strip()
-            bookParseTree = parser.Book()
-            bookParseTree.children.append(uparser.parseString(title=article, wikidb=w['wiki']))
-            metabook.addArticles([article])
-        else:
-            metabook.readJsonFile(metabookfile)
-            if cp.has_section('pdf'):
-                coverimage = cp.get('pdf','coverimage',None)
-            bookParseTree = buildBook(metabook, w['wiki'])
+    licensearticle = cp.get('wiki', 'defaultarticlelicense')
+    if licensearticle:
+        license = uparser.parseString(title=licensearticle, wikidb=w['wiki'])
+        
+    metabook.source = {
+        'name': cp.get('wiki', 'name'),
+        'url': cp.get('wiki', 'url'),
+        'defaultarticlelicense': license,
+    }    
+    coverimage = None
+    if not metabookfile:
+        article = unicode(args[0],'utf-8').strip()
+        bookParseTree = parser.Book()
+        bookParseTree.children.append(uparser.parseString(title=article, wikidb=w['wiki']))
+        metabook.addArticles([article])
+    else:
+        metabook.readJsonFile(metabookfile)
+        if cp.has_section('pdf'):
+            coverimage = cp.get('pdf','coverimage',None)
+        bookParseTree = buildBook(metabook, w['wiki'])
 
-        if not output: #fixme: this only exists for debugging purposes
-            output = 'test.pdf'
-        if not removedarticles and output: # FIXME
-            removedarticles = output + ".removed"
-            
-        r.writeBook(metabook, bookParseTree, output=output + '.tmp', removedArticlesFile=removedarticles, coverimage=coverimage)
-            
-        os.rename(output + '.tmp', output)
-    except Exception, e:
-        if options.errorfile:
-            errorfile = open(options.errorfile, 'w')
-            print 'writing errors to %r' % options.errorfile
-            errorfile.write('Caught: %s %s' % (e, type(e)))
-        else:
-            raise
+    if not output: #fixme: this only exists for debugging purposes
+        output = 'test.pdf'
+    if not removedarticles and output: # FIXME
+        removedarticles = output + ".removed"
+
+    r.writeBook(metabook, bookParseTree, output=output + '.tmp', removedArticlesFile=removedarticles, coverimage=coverimage)
+
+    os.rename(output + '.tmp', output)
     
 def pdfcollection():
     optparser = optparse.OptionParser(usage="%prog [-c CONFIG] [-d] [-o OUTPUT] COLLECTIONTITLE")
