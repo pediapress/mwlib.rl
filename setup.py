c21b386128c442818e050a0780621872d16d01b6
ralf@brainbot.com <ralf@brainbot.com>
2008-08-01 15:06:13 +0200

    automatically install mwlib.ext

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ version=None
 execfile(distutils.util.convert_path('mwlib/rl/__init__.py')) 
 # adds 'version' to local namespace
 
-install_requires=["mwlib>=0.8.0.dev, <0.9.0", "pygments>=0.10"]
+install_requires=["mwlib>=0.8.0.dev, <0.9.0", "pygments>=0.10", "mwlib.ext>=0.8.2"]
 
 def read_long_description():
     fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
