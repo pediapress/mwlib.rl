0d07f783b4dddd30fe21bd4bea75d1f1472fb96a
johannes.beigel@brainbot.com <johannes.beigel@brainbot.com>
2008-10-20 18:22:58 +0200

    run make if this is a hg checkout

--- setup.py
+++ setup.py
@@ -19,21 +19,30 @@ def read_long_description():
     fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
     return open(fn).read()
 
-setup(
-    name="mwlib.rl",
-    version=str(version),
-    entry_points = {
-        'mwlib.writers': ['rl = mwlib.rl.rlwriter:writer'],
-    },
-    install_requires=install_requires,
-    packages=["mwlib", "mwlib.rl", "mwlib.fonts"],
-    namespace_packages=['mwlib'],
-    zip_safe=False,
-    include_package_data=True,
-    url = "http://code.pediapress.com/",
-    description="generate pdfs from mediawiki markup",
-    long_description = read_long_description(),
-    license="BSD License",
-    maintainer="pediapress.com",
-    maintainer_email="info@pediapress.com",
-)
+def main():
+    if os.path.exists(distutils.util.convert_path('Makefile')):
+        # this is a hg checkout
+        print 'Running make'
+        os.system('make')
+    
+    setup(
+        name="mwlib.rl",
+        version=str(version),
+        entry_points = {
+            'mwlib.writers': ['rl = mwlib.rl.rlwriter:writer'],
+        },
+        install_requires=install_requires,
+        packages=["mwlib", "mwlib.rl", "mwlib.fonts"],
+        namespace_packages=['mwlib'],
+        zip_safe=False,
+        include_package_data=True,
+        url = "http://code.pediapress.com/",
+        description="generate pdfs from mediawiki markup",
+        long_description = read_long_description(),
+        license="BSD License",
+        maintainer="pediapress.com",
+        maintainer_email="info@pediapress.com",
+    )
+
+if __name__ == '__main__':
+    main()
