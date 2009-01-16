9e9d70b99ab9cd1880e744aecb8ba8873b93af51
volker@brainbot.com <volker@brainbot.com>
2009-01-16 18:03:07 +0100

    add translatable strings

---
+++ mwlib/rl/pdfmessages.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+#! -*- coding:utf-8 -*-
+
+# Copyright (c) 2007, PediaPress GmbH
+# See README.txt for additional licensing information.
+
+
+# This file is used to store translatable messages. The messages will probably be used in the future in the PDF.
+# Currently they are only used in the printed version.
+
+
+_('Article Licenses')
+
+_('Title')
+
+_('Subtitle:')
+
+_('Editor:')
+
+_('Created on:')
+
+_('Contents')
+
+_('Articles')
+
+_('Produced by:')
+
+_('pdf writer version:')
+
+_('mwlib version:')
+
+_('Table of Contents')
+
+_('Skipping Articles!')
+
+_('Please note:  This is only a preview intended to give an idea how the book is typeset.')
+
+_('References')
+
+_('The following sections list the sources for all articles and images used in this book. Those sources provide more detailed licensing information including the copyright status, the copyright owner, and the license conditions.')
+
+_('Article Sources, Licenses and Contributors')
+
+_('Image Sources, Licenses and Contributors')
+
+_('Image')
