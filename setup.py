c2e36fb761082e3025b096ccb9f7e9d0c52070a1
Ralf Schmitt <ralf@brainbot.com>
2009-08-25 13:59:24 +0200

    mwlib.ext 0.12 is fine

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ version=None
 execfile(distutils.util.convert_path('mwlib/rl/_version.py')) 
 # adds 'version' to local namespace
 
-install_requires=["mwlib>=0.12.3, <0.13", "pygments>=0.10", "mwlib.ext>=0.9.3, <0.10"]
+install_requires=["mwlib>=0.12.3, <0.13", "pygments>=0.10", "mwlib.ext>=0.9.3, <0.13"]
 
 def read_long_description():
     fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
