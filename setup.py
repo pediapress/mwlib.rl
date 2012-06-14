d1065aa6a9321cde6d46138a3df9aee10cff6ba0
Ralf Schmitt <ralf@systemexit.de>
2012-06-14 12:50:58 +0200

    check that .mo files used for translation are included when running sdist
    we use a custom sdist command which ensures that the number of .mo and
    .po files match.
    
    This prevents https://bugzilla.wikimedia.org/show_bug.cgi?id=37544
    from happening again.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Copyright (c) PediaPress GmbH
 # See README.txt for additional licensing information.
 
-import os
+import os, sys
 
 try:
     from setuptools import setup, Extension
@@ -12,7 +12,22 @@ except ImportError:
     ez_setup.use_setuptools()
     from setuptools import setup, Extension
 
+from distutils.command.sdist import sdist as _sdist
+
 install_requires = ["mwlib>=0.12.14, <0.14", "pygments>=1.0", "mwlib.ext>=0.9.3, <0.14"]
+
+
+class sdist(_sdist):
+    def run(self):
+        r = _sdist.run(self)
+
+        files = self.filelist.files
+        pofiles = [x for x in files if x.endswith(".po")]
+        mofiles = [x for x in files if x.endswith(".mo")]
+        if len(pofiles) != len(mofiles):
+            sys.exit("sdist: .mo/.po files mismatch: %s .mo files, %s .po files" % (len(mofiles), len(pofiles)))
+
+        return r
 
 
 def get_version():
@@ -33,6 +48,7 @@ def main():
         entry_points={
             'mwlib.writers': ['rl = mwlib.rl.rlwriter:writer'],
         },
+        cmdclass=dict(sdist=sdist),
         install_requires=install_requires,
         packages=["mwlib", "mwlib.rl", "mwlib.fonts"],
         namespace_packages=['mwlib'],
